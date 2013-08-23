d02856941de6581494169beeeee7953982ab8ad4
Dan Langford <danlangford@gmail.com>
2013-08-23 12:50:43 -0600

    some polish around the nav menu setup

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,50 @@
-var app = angular.module('app', ['ngCookies'], ['$routeProvider', function($routeProvider){
-    $routeProvider.when('/', {
+var app = angular.module('app', ['ngCookies'])
+
+//
+// ROUTES
+//
+.config(['$routeProvider', function($routeProvider){
+    
+    $routeProvider
+    
+    .when('/', {
         templateUrl: 'app/partials/home.html',
         controller: 'HomeController'
-    });
-
-    $routeProvider.when('/aem', {
+    })
+    
+    .when('/quickstart/android', {
+        templateUrl: 'app/partials/quickstart/android.html'
+    })
+    
+    .when('/quickstart/ios', {
+        templateUrl: 'app/partials/quickstart/ios.html'
+    })
+    
+    .when('/aem', {
         templateUrl: 'app/partials/aem/aem-intro.html',
         link: {
             prev: false,
             next: '/aem/ios/apns',
             nextText: 'APNS'
         }
-    });
-
-    $routeProvider.when('/aem/ios/apns', {
+    })
+    
+    .when('/aem/ios/apns', {
         templateUrl: 'app/partials/aem/ios/apns.html',
         link: {
             prev: '/aem',
             prevText: 'AEM',
             next: ''
         }
-    });
+    })
+    
+    .otherwise({redirectTo: '/'});
 
-}]);
+}])
+
+.run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function () {
+        $('.nav-selected').removeClass('nav-selected');
+        $('.nav.bs-sidenav a[href="#'+$location.path()+'"]').addClass('nav-selected')
+    })
+});
