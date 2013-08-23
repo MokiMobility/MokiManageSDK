899af62cc417f0bc11705e711b0431fbab63dc80
justinblake <justin.blake.dev@gmail.com>
2013-08-23 07:31:44 -0600

    menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,17 @@ app.controller('MainController', function($scope) {
 
     $scope.$on('$routeChangeSuccess', function (scope, next, current) {
 
-        console.log(current)
+        if (!next)
+            return;
+
+        if (next.$$route.templateUrl.indexOf('home.html') != -1) {
+
+
+
+            $scope.home = true
+
+        } else
+            $scope.home = false
 
     });
 
