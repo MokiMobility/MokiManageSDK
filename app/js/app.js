41490518c7aac8ca048ef5b56228dec7781650fd
Dan Langford <danlangford@gmail.com>
2013-08-23 08:45:31 -0600

    update some routing. the site is deployed in a subdir

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
 var app = angular.module('app', [], ['$routeProvider', function($routeProvider){
     $routeProvider.when('/', {
-        templateUrl: '/app/partials/home.html',
+        templateUrl: 'app/partials/home.html',
         controller: 'HomeController'
     });
 
     $routeProvider.when('/aem', {
-        templateUrl: '/app/partials/aem/aem-intro.html',
+        templateUrl: 'app/partials/aem/aem-intro.html',
         link: {
             prev: false,
             next: '/aem/ios/apns',
@@ -14,7 +14,7 @@ var app = angular.module('app', [], ['$routeProvider', function($routeProvider){
     });
 
     $routeProvider.when('/aem/ios/apns', {
-        templateUrl: '/app/partials/aem/ios/apns.html',
+        templateUrl: 'app/partials/aem/ios/apns.html',
         link: {
             prev: '/aem',
             prevText: 'AEM',
