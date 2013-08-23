be364f854fd8c24346a1a4e227b6d1d57b06974f
Dan Langford <danlangford@gmail.com>
2013-08-23 16:13:55 -0600

    syntax highlighting, quick start guides, and some polish

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,10 +1,13 @@
 app.directive('contextSwitcher', function(){
     return {
-        controller: function($scope, $cookieStore){
-            $scope.context = $cookieStore.get('context') || 'apple';  
-            $scope.contextSwitch=function(ctx){
-                $scope.context = ctx;
+        controller: function($rootScope, $cookieStore, $location){
+            $rootScope.context = $cookieStore.get('context') || 'apple';  
+            $rootScope.contextSwitch=function(ctx){
+                $rootScope.context = ctx;
                 $cookieStore.put('context',ctx);
+                if($location.path().indexOf("quickstart")>=0){
+                    $location.path("/quickstart/"+ctx);
+                }
             }
         },
         template: '<div class="btn-group"><button class="btn btn-default" ng-click="contextSwitch(\'android\')" context-active="android"><i class="icon-android icon-large"></i></button><button class="btn btn-default" context-active="apple" ng-click="contextSwitch(\'apple\')"><i class="icon-apple icon-large"></i></button></div>',
@@ -41,5 +44,12 @@ app.directive('contextSwitcher', function(){
     return {
         template: '<i class="icon-{{context}}"></i>',
         restrict : 'C'
+    };
+}).directive('code',function(){
+    return {
+        restrict:'E',
+        link: function(scope, iElement, iAttrs, controller){
+                hljs.highlightBlock(iElement.get(0));
+            }
     };
 });
