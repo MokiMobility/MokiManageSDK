a720b9e487c7cd91649c2d951a7854b2cab03200
Justin Blake <justin.blake.dev@gmail.com>
2013-12-11 05:11:53 -0700

    1.2 Documentation Updates

--- Data/Toc.js
+++ Data/Toc.js
@@ -1,1 +1,35 @@
-define({numchunks:1,prefix:'Toc_Chunk',chunkstart:['/Content/Introduction_to_the_MokiManage_SDK.htm'],tree:{n:[{i:0,c:0},{i:1,c:0,n:[{i:2,c:0},{i:3,c:0,n:[{i:4,c:0},{i:5,c:0},{i:6,c:0},{i:7,c:0},{i:8,c:0},{i:9,c:0}]},{i:10,c:0},{i:11,f:'_blank',c:0}]},{i:12,c:0,n:[{i:13,c:0},{i:14,c:0,n:[{i:15,c:0},{i:16,c:0},{i:17,c:0},{i:18,c:0},{i:19,c:0},{i:20,c:0}]},{i:21,c:0},{i:22,f:'_blank',c:0}]},{i:23,c:0}]}});
+define({numchunks: 1, prefix: 'Toc_Chunk', chunkstart: ['/Content/Introduction_to_the_MokiManage_SDK.htm'], tree: {n: [
+    {i: 0, c: 0},
+    {i: 1, c: 0, n: [
+        {i: 2, c: 0},
+        {i: 3, c: 0, n: [
+            {i: 4, c: 0},
+            {i: 5, c: 0},
+            {i: 6, c: 0},
+            {i: 7, c: 0},
+            {i: 8, c: 0},
+            {i: 9, c: 0},
+            {i: 10, c: 0},
+            {i: 11, c: 0}
+        ]},
+        {i: 12, c: 0},
+        {i: 13, f: '_blank', c: 0}
+    ]},
+    {i: 14, c: 0, n: [
+        {i: 15, c: 0},
+        {i: 16, c: 0, n: [
+            {i: 17, c: 0},
+            {i: 18, c: 0},
+            {i: 19, c: 0},
+            {i: 20, c: 0},
+            {i: 21, c: 0},
+            {i: 22, c: 0},
+            {i: 23, c: 0},
+            {i: 24, c: 0},
+            {i: 25, c: 0}
+        ]},
+        {i: 26, c: 0},
+        {i: 27, f: '_blank', c: 0}
+    ]},
+    {i: 28, c: 0}
+]}});
